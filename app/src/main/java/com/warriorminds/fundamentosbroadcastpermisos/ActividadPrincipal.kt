1b4144e6413b8fcc223bd890340f479b5e7c00dd
Rodrigo Guerrero <rodrigo.x.guerero.-ND@disney.com>
2018-01-18 10:55:26 -0600

    app inicial

---
+++ app/src/main/java/com/warriorminds/fundamentosbroadcastpermisos/ActividadPrincipal.kt
@@ -0,0 +1,12 @@
+package com.warriorminds.fundamentosbroadcastpermisos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActividadPrincipal : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.actividad_principal)
+    }
+}
